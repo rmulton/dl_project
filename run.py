87e0dc4f1ffc59e6c6fd0e46cbeb0330153dc8c5
unknown <titouan.poisson@student.ecp.fr>
2018-04-09 17:39:21 +0200

    cuda

--- run.py
+++ run.py
@@ -47,8 +47,8 @@ def training(epochs, trainloader, evaloader, optimizer, net, current_epoch, crit
             inputs, labels = data
 
             # wrap them in Variable
-            inputs, labels = Variable(inputs), Variable(labels)
-            # inputs, labels = Variable(inputs).cuda(), Variable(labels).cuda()
+            # inputs, labels = Variable(inputs), Variable(labels)
+            inputs, labels = Variable(inputs).cuda(), Variable(labels).cuda()
 
             # zero the parameter gradients
             optimizer.zero_grad()
