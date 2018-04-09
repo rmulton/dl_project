20ce72f883755292d809c1b5dbc35d80dfdc5e9d
Alexandre Sioufi <sioufia78@gmail.com>
2018-04-09 16:42:02 +0200

    Added

--- run.py
+++ run.py
@@ -118,7 +118,9 @@ def training(epochs, trainloader, evaloader, optimizer, net, current_epoch, crit
 def launch_training(resuming=False, input_type=0, *args):
     """Function that configurates the model from init or a last model ; and then it trains the model"""
     print(resuming)
-    epochs, trainloader, evaloader, optimizer, net, current_epoch, criterion, evalset_length, evalset = conf_training(resuming=resuming,input_type=input_type, *args)
+    print(input_type)
+    print(args[0])
+    epochs, trainloader, evaloader, optimizer, net, current_epoch, criterion, evalset_length, evalset = conf_training(resuming=resuming, input_type=input_type, args[0])
     training(epochs, trainloader, evaloader, optimizer, net, current_epoch, criterion, evalset_length, evalset)
 
 def launch_testing(model_epoch, input_type=0):
